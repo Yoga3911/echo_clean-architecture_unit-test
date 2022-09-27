f611d117dd9066065db1aa039396de349f8052e6
Robin Hood <yoga3911@gmail.com>
2022-09-27 23:06:40 +0700

    use env for db credential

--- configs/db-config.go
+++ configs/db-config.go
@@ -2,7 +2,10 @@ package configs
 
 import (
 	"fmt"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 
@@ -18,12 +21,17 @@ type Config struct {
 }
 
 func InitDB() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
 	config := Config{
-		DB_Username: "root",
-		DB_Password: "123456",
-		DB_Port:     "3356",
-		DB_Host:     "localhost",
-		DB_Name:     "gorm_crud",
+		DB_Username: os.Getenv("DB_HOST"),
+		DB_Password: os.Getenv("DB_USERNAME"),
+		DB_Port:     os.Getenv("DB_PASSWORD"),
+		DB_Host:     os.Getenv("DB_NAME"),
+		DB_Name:     os.Getenv("DB_PORT"),
 	}
 
 	connectionString := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local",
@@ -34,7 +42,6 @@ func InitDB() *gorm.DB {
 		config.DB_Name,
 	)
 
-	var err error
 	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		panic(err)
